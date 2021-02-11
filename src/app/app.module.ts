119e3fa93770373765ac92c8e1fa459bcf806882
dhruvasanth <sayanadhruvasanth@gmail.com>
2021-02-11 10:26:44 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponentComponent } from './home-page-component/home-page-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
